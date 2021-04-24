0337d380e5c70f0a94fb163d793eb0c37e132d88
Your Name <you@example.com>
2021-04-24 15:18:20 +0300

    24.04

--- forward/test/views.py
+++ forward/test/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
-def index(request):
-    return render(request, 'test/index.html')
+# def index(request):
+#     return render(request, 'test/index.html')
+
+def home(request):
+    return HttpResponse('<h2>hi</h2>')
